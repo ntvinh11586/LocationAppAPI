762ba014d81fede1bab5a7c9685d21d8ad8a711e
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-07 06:41:11 +0700

    Update convention

--- app/socket/index.js
+++ app/socket/index.js
@@ -5,16 +5,17 @@ module.exports = (io, app) => {
   let allrooms = app.locals.chatrooms;
   let location = app.locals.location;
 
-  // route: /locations - type: connection
+  // Route: /locations - Type namespace: connection
   io.of('/locations').on('connection', socket => {
-    // Usecase: get current location
+    // Usecase: Get current location.
     // on: getCurrentLocation -> emit: getLocation
     socket.on('getCurrentLocation', () => {
       socket.emit('getLocation', JSON.stringify(location));
     });
-
+    // Usecase: Create new location and broadcast.
+    // on: getCurrentLocation -> emit: getLocation
     socket.on('newLocation', newLocation => {
-      location = { x: newLocation.x, y: newLocation.y };
+      location = newLocation;
       socket.emit('getLocation', JSON.stringify(location));
       socket.broadcast.emit('getLocation', JSON.stringify(location));
     });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const app = express();
-const chatCat = require('./app');
+const travelAppAPI = require('./app');
 const passport = require('passport');
 
 app.set('port', process.env.PORT || 3000);
@@ -9,12 +9,12 @@ app.use(express.static('public'));
 // app.set('views', './views'); // views = ./views
 app.set('view engine', 'ejs');
 
-app.use(chatCat.sessions);
+app.use(travelAppAPI.sessions);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', chatCat.router);
+app.use('/', travelAppAPI.router);
 
-chatCat.ioServer(app).listen(app.get('port'), () => {
-  console.log('CHATCAT is running on Port: ', app.get('port'));
+travelAppAPI.ioServer(app).listen(app.get('port'), () => {
+  console.log('travelAppAPI is running on Port: ', app.get('port'));
 });
