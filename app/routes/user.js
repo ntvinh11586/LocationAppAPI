11a33c5b543febe73ae0024a89604ea7be8ffbee
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-26 11:04:47 +0700

    Remove log

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,13 +6,11 @@ const db = require('../db');
 
 router.get('/info', (req, res) => {
   var token = req.query.token;
-  console.log(token);
   jwt.verify(token, 'supersecret', (err, decoded) => {
     if (err) {
       res.send(err);
     } else {
       var _id = req.query._id;
-      console.log(_id);
       db.userModel.findById(_id, (err, user) => {
         if (err) {
           res.send(err);
