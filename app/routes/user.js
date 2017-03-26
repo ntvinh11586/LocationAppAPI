a5f1a832760904faf0247391cd65cd04c1e53515
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-26 17:03:54 +0700

    Modify routes in user

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,18 +4,17 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const db = require('../db');
 
-router.get('/info', (req, res) => {
+router.get('/:id', (req, res) => {
   var token = req.query.token;
   jwt.verify(token, 'supersecret', (err, decoded) => {
     if (err) {
       res.send(err);
     } else {
-      var _id = req.query._id;
+      var _id = req.params.id;
       db.userModel.findById(_id, (err, user) => {
         if (err) {
           res.send(err);
         } else {
-          console.log(user);
           var user = {
             _id: user._id,
             username: user.username
