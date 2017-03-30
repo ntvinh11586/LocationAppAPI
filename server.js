be41c17f85740746ad3f29b2fe460a778f40a304
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-30 21:01:57 +0700

    Refactor server.js

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const passport = require('passport');
 const locationAppAPI = require('./app');
 const bodyParser = require('body-parser');
+
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
