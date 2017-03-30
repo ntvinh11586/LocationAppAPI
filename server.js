c8f5e724ddccdfa690759a312f3bf1b993e44a87
tiendat171195 <tiendat.dee.95@gmail.com>
2017-03-30 09:48:50 +0700

    Add json parser

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const passport = require('passport');
 const locationAppAPI = require('./app');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
