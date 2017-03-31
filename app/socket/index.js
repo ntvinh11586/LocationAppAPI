109116da974c2ee27956cd9be0c9dd9acefe865c
ntvinh11586 <ntvinh.11586@gmail.com>
2017-03-31 22:50:54 +0700

    Refactor

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,6 +1,43 @@
 const db = require('../db');
 
 module.exports = (io, app) => {
+  io.of('/location_all_user').on('connection', (socket) => {
+    socket.on('get_location', (userId) => {
+      db.latlngModel.find({}, (err, latlng) => {
+        if (err) {
+          console.log(latlng);
+        }
+        socket.emit('get_location_callback', JSON.stringify(latlng));
+      });
+    });
+
+    socket.on('set_location', (newLatlng) => {
+      db.latlngModel.findOne({ _user_id: newLatlng._user_id }, (err, latlng) => {
+        if (err) {
+          socket.emit('get_location_callback', JSON.stringify(latlng));
+          socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
+        } else if (latlng) {
+          latlng.longitude = newLatlng.longitude;
+          latlng.latitude = newLatlng.latitude;
+          latlng.save();
+          socket.emit('get_location_callback', JSON.stringify(latlng));
+          socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
+        } else {
+          db.latlngModel.create(newLatlng, (err, latlng) => {
+            if (err) {
+              console.log(err);
+              socket.emit('get_location_callback', JSON.stringify(latlng));
+              socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
+            } else {
+              socket.emit('get_location_callback', JSON.stringify(latlng));
+              socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
+            }
+          });
+        }
+      });
+    });
+  });
+
   io.of('/location_one_user').on('connection', (socket) => {
     socket.on('get_location', (userId) => {
       // find userId in db
