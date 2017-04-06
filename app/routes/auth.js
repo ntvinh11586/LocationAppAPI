09460e1b001592ab995298db17a5226b1677d1d8
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-06 10:39:50 +0700

    Refactor code

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -10,21 +10,19 @@ router.post('/register', (req, res) => {
 
   db.UserModel.findOne({ username }, (err, hasAccount) => {
     if (err) {
-      res.json({status: 'error', message: err});
+      res.json({ status: 'error', message: err });
+    } else if (hasAccount) {
+        res.json({ status: 'error', message: 'Acount already exists!' });
     } else {
-      if (hasAccount) {
-        res.json({status: 'error', message: 'Acount already exists!'});
-      } else {
-        db.UserModel.create({username, password}, (err, account) => {
-          var token = jwt.sign({username: account.username}, 'supersecret', {expiresIn: 10000});
-          var userInfo = {
-            _id: account._id,
-            username: account.username,
-            token: token
-          }
-          res.json({status: 'success', message: 'Congratulations! Your account has been successfully created!', userInfo: userInfo});
-        });
-      }
+      db.UserModel.create({ username, password }, (err, account) => {
+        const token = jwt.sign({ username: account.username }, 'supersecret', { expiresIn: 10000 });
+        const userInfo = {
+          _id: account._id,
+          username: account.username,
+          token,
+        };
+        res.json({ status: 'success', message: 'Congratulations! Your account has been successfully created!', userInfo });
+      });
     }
   });
 });
@@ -34,27 +32,24 @@ router.post('/login', (req, res) => {
   const password = req.body.password;
   db.UserModel.findOne({ username, password }, (err, hasAccount) => {
     if (err) {
-      res.json({status: 'error', message: err});
+      res.json({ status: 'error', message: err });
+    } else if (hasAccount) {
+      const token = jwt.sign({ _id: hasAccount._id }, 'supersecret', { expiresIn: 10000 });
+      const userInfo = {
+        _id: hasAccount._id,
+        username,
+        token,
+      };
+      res.json({ status: 'success', message: "You've successfully logged in!", userInfo });
     } else {
-      if (hasAccount) {
-        var _id = hasAccount._id;
-        var token = jwt.sign({_id}, 'supersecret', {expiresIn: 10000});
-        var userInfo = {
-          _id: _id,
-          username: username,
-          token: token
-        }
-        res.json({status: 'success', message: "You've successfully logged in!", userInfo: userInfo});
-      } else {
-        res.json({status: 'error', message: "Username or password is incorrect!"});
-      }
+      res.json({ status: 'error', message: 'Username or password is incorrect!' });
     }
   });
 });
 
-router.get('/logout', (req, res, next) => {
-    req.logout();
-    res.json({status: 'success', message: "Logged Out!"});
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.json({ status: 'success', message: 'Logged Out!' });
 });
 
 module.exports = router;
