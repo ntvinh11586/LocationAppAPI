5487879e3af53b270d123e91e3ae272221484af0
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-22 20:28:30 +0700

    Remove something

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const passport = require('passport');
 const locationAppAPI = require('./app');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.set('port', process.env.PORT || 3000);
-
 app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-
-// Configure passport.js
-app.use(passport.initialize());
-app.use(passport.session());
 
 // routes' pointer
 app.use('/', locationAppAPI.router);
