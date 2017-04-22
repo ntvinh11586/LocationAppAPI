4e14ed439c93d03e564d6dfe5aa2bb92275721f3
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-22 16:40:46 +0700

    Handle events from map sockets (v0.4.0)

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,6 +1,50 @@
 const db = require('../db');
 
 module.exports = (io, app) => {
+  io.of('/group_location').on('connection', (socket) => {
+    socket.on('update_new_user_location', (newLocationInfo) => {
+      const newLocationInfoJSON = JSON.parse(newLocationInfo);
+      const userId = newLocationInfoJSON._id;
+      const latlng = newLocationInfoJSON.latlng;
+      console.log(newLocationInfoJSON);
+      db.UserModel.findById(userId, (err, user) => {
+        if (err) {
+          console.log('err');
+        } else if (user == null) {
+          console.log('no users');
+        } else {
+          user.latlng.lat = latlng.lat;
+          user.latlng.lng = latlng.lng;
+          user.save();
+          socket.emit('update_new_user_location_callback', newLocationInfo);
+          socket.broadcast.emit('update_new_user_location_callback', newLocationInfo);
+        }
+      });
+    });
+
+    socket.on('get_all_users_location', (groupInfo) => {
+      const groupJSON = JSON.parse(groupInfo);
+      const groupId = groupJSON.id;
+      db.GroupModel.findById(groupId).populate('users').populate('latlng').exec((err, group) => {
+        if (err) {
+          console.log('err');
+        } else if (group == null) {
+          console.log('no group');
+        } else {
+          const users = group.users;
+          let locations = [];
+          for (let i = 0; i < users.length; i += 1) {
+            if (users[i].latlng != null) {
+              locations.push({ _id: users[i]._id, latlng: users[i].latlng });
+            }
+          }
+          socket.emit('get_all_users_location_callback', { latlng: locations });
+        }
+      });
+    });
+  });
+
+
   io.of('/location_all_user').on('connection', (socket) => {
     socket.on('get_location', (userId) => {
       // we use userId for authorization.
