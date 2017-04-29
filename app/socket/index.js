7fc64e89370b56f7ca8c03a2c5d8e95c2a81f9af
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-29 15:46:45 +0700

    Remove duplcated socket folder

--- app/socket/index.js
+++
@@ -1,7 +0,0 @@
-const groupLocation = require('./group_location');
-const groupMessenger = require('./group_messenger');
-
-module.exports = (io) => {
-  groupLocation(io);
-  groupMessenger(io);
-};
