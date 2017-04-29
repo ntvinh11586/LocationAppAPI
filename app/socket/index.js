9894a1bb2164c665f2c142ba7761579679f2a325
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-29 09:37:05 +0700

    Refactor socket

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,6 +1,6 @@
 const db = require('../db');
 
-module.exports = (io) => {
+function groupLocation(io) {
   io.of('/group_location').on('connection', (socket) => {
     socket.on('update_new_user_location', (newLocationInfo) => {
       const newLocationInfoJSON = JSON.parse(newLocationInfo);
@@ -44,56 +44,8 @@ module.exports = (io) => {
       });
     });
   });
+}
 
-
-  io.of('/location_all_user').on('connection', (socket) => {
-    socket.on('get_location', (userId) => {
-      // we use userId for authorization.
-      db.latlngModel.find({}, (err, latlng) => {
-        if (err) {
-          console.log(latlng);
-        }
-        socket.emit('get_location_callback', { user_locations: latlng });
-      });
-    });
-  });
-
-  io.of('/location_one_user').on('connection', (socket) => {
-    socket.on('get_location', (userId) => {
-      // find userId in db
-      // emit {x, y} to users
-      db.latlngModel.findOne({ _user_id: userId }, (err, latlng) => {
-        if (err) {
-          console.log(latlng);
-        }
-        socket.emit('get_location_callback', JSON.stringify(latlng));
-      });
-    });
-
-    socket.on('set_location', (newLatlng) => {
-      db.latlngModel.findOne({ _user_id: newLatlng._user_id }, (err, latlng) => {
-        if (err) {
-          socket.emit('get_location_callback', JSON.stringify(latlng));
-          socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
-        } else if (latlng) {
-          latlng.longitude = newLatlng.longitude;
-          latlng.latitude = newLatlng.latitude;
-          latlng.save();
-          socket.emit('get_location_callback', JSON.stringify(latlng));
-          socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
-        } else {
-          db.latlngModel.create(newLatlng, (err, latlng) => {
-            if (err) {
-              console.log(err);
-              socket.emit('get_location_callback', JSON.stringify(latlng));
-              socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
-            } else {
-              socket.emit('get_location_callback', JSON.stringify(latlng));
-              socket.broadcast.emit('get_location_callback', JSON.stringify(latlng));
-            }
-          });
-        }
-      });
-    });
-  });
+module.exports = (io) => {
+  groupLocation(io);
 };
