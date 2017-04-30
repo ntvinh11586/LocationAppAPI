b6f4ce53d939a67a1ef59d306e84ca36b0971172
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-30 15:38:48 +0700

    Work for comment

---
+++ app/models/comment.js
@@ -0,0 +1,28 @@
+const db = require('../db');
+
+function createComment(newfeedId, userId, description, callback) {
+  db.newfeedModel.findById(newfeedId, (err, newfeed) => {
+    if (err) {
+      callback(err, { err });
+    } else {
+      db.commentModel.create({ description }, (err, comment) => {
+        if (err) {
+          callback(err, { err });
+        } else {
+          // Add author in comment
+          comment.author._id = userId;
+          comment.save();
+          // Add comment in newsfeed
+          newfeed.comments.push(comment);
+          newfeed.save();
+
+          callback(null, comment);
+        }
+      });
+    }
+  });
+}
+
+module.exports = {
+  createComment
+}
