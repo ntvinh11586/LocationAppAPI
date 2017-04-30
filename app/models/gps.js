f422b4082d56eff8b472b4fa19992139da928314
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-30 16:17:56 +0700

    Work for gps

---
+++ app/models/gps.js
@@ -0,0 +1,33 @@
+const db = require('../db');
+
+function createCurrentLatlng(userId, lat, lng, callback) {
+  db.UserModel.findById(userId, (err, user) => {
+    if (err) {
+      callback(err, { err: 'err' });
+    } else if (user == null) {
+      callback(null, { err: 'Cannot find users' });
+    } else {
+      user.latlng.lng = lng;
+      user.latlng.lat = lat;
+      user.save();
+      callback(null, { lat, lng });
+    }
+  });
+}
+
+function getPreviousLatlng(userId, callback) {
+  db.UserModel.findById(userId, (err, user) => {
+    if (err) {
+      callback(err, { err: 'err' });
+    } else if (user == null) {
+      callback(null, { err: 'Cannot find users' });
+    } else {
+      callback(null, user.latlng);
+    }
+  });
+}
+
+module.exports = {
+  createCurrentLatlng,
+  getPreviousLatlng,
+};
