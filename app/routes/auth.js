280ede1e1483e80583a35ab8800adc0c2fc7bf94
ntvinh11586 <ntvinh.11586@gmail.com>
2017-04-30 14:57:09 +0700

    Separate logics out of controllers

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,44 +1,29 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const db = require('../db');
-const deprecate = require('deprecate');
 
 const router = express.Router();
 
-// Depricated router
-router.post('/register', (req, res) => {
-  deprecate("router.post('/register') is deprecated");
-
-  const username = req.body.username;
-  const password = req.body.password;
-
+function database1(username, password, callback) {
   db.UserModel.findOne({ username }, (err, hasAccount) => {
     if (err) {
-      res.json({ status: 'error', message: err });
-    }   else if (hasAccount) {
-        res.json({ status: 'error', message: 'Acount already exists!' });
+      callback(err, { status: 'error', message: err });
+    } else if (hasAccount) {
+      callback(null, { status: 'error', message: 'Acount already exists!' });
     } else {
       db.UserModel.create({ username, password }, (err, account) => {
         const token = jwt.sign({ username: account.username }, 'supersecret', { expiresIn: 10000 });
-        const userInfo = {
-          _id: account._id,
-          username: account.username,
-          token,
-        };
-        res.json({ status: 'success', message: 'Congratulations! Your account has been successfully created!', userInfo });
+        const userInfo = { _id: account._id, username: account.username, token };
+        callback(null, { status: 'success', message: 'Congratulations! Your account has been successfully created!', userInfo });
       });
     }
   });
-});
+}
 
-router.post('/login', (req, res) => {
-  deprecate("router.post('/login') is deprecated");
-
-  const username = req.body.username;
-  const password = req.body.password;
+function database2(username, password, callback) {
   db.UserModel.findOne({ username, password }, (err, hasAccount) => {
     if (err) {
-      res.json({ status: 'error', message: err });
+      callback(err, { status: 'error', message: err });
     } else if (hasAccount) {
       const token = jwt.sign({ _id: hasAccount._id }, 'supersecret', { expiresIn: 10000 });
       const userInfo = {
@@ -46,17 +31,37 @@ router.post('/login', (req, res) => {
         username,
         token,
       };
-      res.json({ status: 'success', message: "You've successfully logged in!", userInfo });
+      callback(null, { status: 'success', message: "You've successfully logged in!", userInfo });
     } else {
-      res.json({ status: 'error', message: 'Username or password is incorrect!' });
+      callback(null, { status: 'error', message: 'Username or password is incorrect!' });
     }
+  });
+}
+
+function database3(callback) {
+  callback(null, { status: 'success', message: 'Logged Out!' });
+}
+
+router.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  database1(username, password, (err, json) => {
+    res.json(json);
+  });
+});
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  database2(username, password, (err, json) => {
+    res.json(json);
   });
 });
 
 router.get('/logout', (req, res) => {
-  deprecate("router.get('/logout') is deprecated");
-
-  res.json({ status: 'success', message: 'Logged Out!' });
+  database3((err, json) => {
+    res.json(json);
+  });
 });
 
 module.exports = router;
