45fccf17e5f8be73c9ed33039217b2036aab50cd
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-02 17:03:56 +0700

    Add the missing

--- src/models/marker.js
+++ src/models/marker.js
@@ -28,7 +28,27 @@ function getMarkers(groupId, callback) {
   });
 }
 
+function deleteMarker(groupId, markerId, callback) {
+  groupRepository.findById(groupId, (err, group) => {
+    if (err) {
+      callback(err, { err: 'err' });
+    } else if (group == null) {
+      callback(null, { err: 'no group' });
+    } else {
+      const markers = group.markers;
+      if (markers.some(x => x._id.equals(markerId))) {
+        group.markers.pull({ _id: markerId });
+        group.save();
+        callback(null, { marker_id: markerId });
+      } else {
+        callback(null, { err: 'no markers' });
+      }
+    }
+  });
+}
+
 module.exports = {
   addMarker,
   getMarkers,
+  deleteMarker,
 };
