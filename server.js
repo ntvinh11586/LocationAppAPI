beafb68f7c1a2efe6eb6bca9036d71fed03959ef
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-07 12:05:28 +0700

    Handle 404

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 
-// routes' pointer
-app.use('/', locationAppAPI.router);
+// Another handle 404 or something error
+// http://stackoverflow.com/a/9802006/5557789
+app.use((req, res, next) => {
+  res.status(404);
+  // respond with json
+  if (req.accepts('json')) {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 
 // Fix deprecation warning mpromise
 // by using the default promise of Node.js.
