07840abc8274a00b8f40243ac25de2eaeb2effbe
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-07 12:14:38 +0700

    Handle 500

--- server.js
+++ server.js
@@ -20,8 +20,13 @@ app.use((req, res, next) => {
   res.status(404);
   // respond with json
   if (req.accepts('json')) {
-    res.status(404).json({ error: 'Not found' });
+    res.status(404).json({ error: 'Not found error - 404' });
   }
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ err: 'Internal error - 500' });
 });
 
 // Fix deprecation warning mpromise
