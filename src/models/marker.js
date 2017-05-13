1f8a34601672717b47eb58bc86de12fc61705740
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-13 16:29:25 +0700

    Unify data response of maker and group

--- src/models/marker.js
+++ src/models/marker.js
@@ -15,14 +15,16 @@ function addMarker(groupId, userId, lat, lng, callback) {
         status_message: 'Group not found.',
       });
     } else {
-      const marker = { lat, lng, user: userId };
+      const marker = {
+        latlng: { lat, lng },
+        user: userId,
+      };
       group.markers.push(marker);
       group.save();
       callback(null, {
         group_id: groupId,
         marker_id: group.markers.slice(-1)[0]._id,
-        lat: group.markers.slice(-1)[0].lat,
-        lng: group.markers.slice(-1)[0].lng,
+        latlng: { lat, lng },
         user: userId,
       });
     }
@@ -30,26 +32,28 @@ function addMarker(groupId, userId, lat, lng, callback) {
 }
 
 function getMarkers(groupId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } else if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      callback(null, {
-        group_id: groupId,
-        markers: group.markers,
-      });
-    }
-  });
+  groupRepository.findById(groupId)
+    .populate({ path: 'markers.user', model: 'User', select: 'username' })
+    .exec((err, group) => {
+      if (err) {
+        callback(err, {
+          status_code: 422,
+          success: false,
+          status_message: err.message,
+        });
+      } else if (group == null) {
+        callback(new Error('422'), {
+          status_code: 422,
+          success: false,
+          status_message: 'Group not found.',
+        });
+      } else {
+        callback(null, {
+          group_id: groupId,
+          markers: group.markers,
+        });
+      }
+    });
 }
 
 function deleteMarker(groupId, markerId, callback) {
