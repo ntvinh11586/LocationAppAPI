0ef98f79050f262a3565c68e78d5962ef62bff7f
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-14 10:30:44 +0700

    Populate user_id for users route (#34)

--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,27 @@
 const userRepository = require('../repositories/user');
 
 function getUserInfo(userId, callback) {
-  userRepository.findById(userId, (err, user) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } else {
-      callback(null, {
-        user_id: user._id,
-        username: user.username,
-        friends: user.friends,
-        friend_pendings: user.friends_pending,
-        friend_requests: user.friend_requests,
-      });
-    }
-  });
+  userRepository.findById(userId)
+    .populate({ path: 'friends', model: 'User', select: 'username' })
+    .populate({ path: 'friend_requests', model: 'User', select: 'username' })
+    .populate({ path: 'friends_pending', model: 'User', select: 'username' })
+    .exec((err, user) => {
+      if (err) {
+        callback(err, {
+          status_code: 422,
+          success: false,
+          status_message: err.message,
+        });
+      } else {
+        callback(null, {
+          user_id: user._id,
+          username: user.username,
+          friends: user.friends,
+          friend_pendings: user.friends_pending,
+          friend_requests: user.friend_requests,
+        });
+      }
+    });
 }
 
 module.exports = {
