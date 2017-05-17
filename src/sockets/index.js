cfe5a0ce4669e828bb601fbcc496669cd3d0409c
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-17 10:52:27 +0700

    Remove personal chat

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,11 +1,9 @@
 const map = require('./map');
 const groupChat = require('./group_chat');
-const personalChat = require('./personal_chat');
 const demo = require('./demo');
 
 module.exports = (io) => {
   map(io);
   groupChat(io);
   demo(io);
-  personalChat(io);
 };
