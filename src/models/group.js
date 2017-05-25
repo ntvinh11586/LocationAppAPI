57fb31e063cc4e989ce4f8ba702ec9d54fbb8c0b
ntvinh11586 <ntvinh.11586@gmail.com>
2017-05-25 23:13:46 +0700

    Implement starting point

--- src/models/group.js
+++ src/models/group.js
@@ -358,7 +358,9 @@ function addArrivingUser(groupId, userId, callback) {
         group.save();
         callback(null, {
           group_id: groupId,
+          name: group.name,
           user_id: userId,
+          username: user.username,
         });
       });
     }
@@ -443,28 +445,35 @@ function getDestinationUsers(groupId, callback) {
 }
 
 function deleteArrivingUser(groupId, userId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else if (group.arriving_users.some(u => u.equals(userId))) {
-      group.arriving_users.pull(userId);
-      group.save();
-      callback(null, {
-        group_id: groupId,
-        user_id: userId,
-      });
-    }
-  });
+  groupRepository.findById(groupId)
+    .populate({ path: 'arriving_users', model: 'User', select: 'username' })
+    .exec((err, group) => {
+      if (err) {
+        callback(err, {
+          status_code: 422,
+          success: false,
+          status_message: err.message,
+        });
+      } if (group == null) {
+        callback(new Error('422'), {
+          status_code: 422,
+          success: false,
+          status_message: 'Group not found.',
+        });
+      } else if (group.arriving_users.some(u => u.equals(userId))) {
+        group.arriving_users.pull(userId);
+        group.save();
+
+        userRepository.findById(userId, (err, user) => {
+          callback(null, {
+            group_id: groupId,
+            name: group.name,
+            user_id: userId,
+            username: user.username,
+          });
+        });
+      }
+    });
 }
 
 function deleteDestinationUser(groupId, userId, callback) {
--- src/sockets/map.js
+++ src/sockets/map.js
@@ -3,6 +3,8 @@ const markerModel = require('../models/marker');
 const groupModel = require('../models/group');
 const socketioJwt = require('socketio-jwt');
 const config = require('../config');
+const notificationDomain = require('../domains/notification');
+const fcmDomain = require('../domains/fcm');
 
 function joinChat(socket, groupId) {
   if (groupId === undefined || groupId === null) {
@@ -325,6 +327,17 @@ function groupLocation(mapNamespace) {
             socket.broadcast
               .to(socket.handshake.query.group_id)
               .emit('add_arriving_user_callback', data);
+
+            notificationDomain.notifyNewMessage(
+              socket.handshake.query.group_id,
+              (err, dTokens) => {
+                fcmDomain.sendMessageToDeviceWithTokens(dTokens.tokens, {
+                  notification: {
+                    title: data.name,
+                    body: `${data.username} is at Starting Point now!`,
+                  },
+                });
+              });
           });
         })
         .on('delete_arriving_user', (groupInfo) => {
@@ -333,6 +346,17 @@ function groupLocation(mapNamespace) {
             socket.broadcast
               .to(socket.handshake.query.group_id)
               .emit('delete_arriving_user_callback', data);
+
+            notificationDomain.notifyNewMessage(
+              socket.handshake.query.group_id,
+              (err, dTokens) => {
+                fcmDomain.sendMessageToDeviceWithTokens(dTokens.tokens, {
+                  notification: {
+                    title: data.name,
+                    body: `${data.username} left Starting Point now!`,
+                  },
+                });
+              });
           });
         })
         .on('get_arriving_users', (groupInfo) => {
