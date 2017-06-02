97634604328348f1f4a34599f7cbaa0287d4d819
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-02 07:09:53 +0700

    Disable deployment mode

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,18 @@
 const development = require('./development.json');
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    host: process.env.host || '',
-    dbURI: process.env.dbURI,
-    tokenSecretKey: process.env.tokenSecretKey,
-    tokenExpired: process.env.tokenExpired,
-    networkTimeout: process.env.networkTimeout,
-  };
-} else {
-  module.exports = development;
-}
+// We will seperate the production & development
+// environment soon, but now we need to focus on
+// deploying on the host.
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = {
+//     host: process.env.host || '',
+//     dbURI: process.env.dbURI,
+//     tokenSecretKey: process.env.tokenSecretKey,
+//     tokenExpired: process.env.tokenExpired,
+//     networkTimeout: process.env.networkTimeout,
+//   };
+// } else {
+//   module.exports = development;
+// }
+
+module.exports = development;
