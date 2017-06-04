7c755e1b655ff4ca3f8f99aa2caa382a7ddeb464
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-04 16:20:59 +0700

    Update none token

--- src/domains/fcm.js
+++ src/domains/fcm.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 
 module.exports = {
-  sendMessageToDeviceWithTokens: (tokens, payload) =>
-    admin.messaging().sendToDevice(tokens, payload),
+  sendMessageToDeviceWithTokens: (tokens, payload) => {
+    if (tokens === undefined || tokens.length === 0) {
+      admin.messaging().sendToDevice(tokens, payload);
+    }
+  },
 };
