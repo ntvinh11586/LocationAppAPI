76cb074aa14baa98843668c26b4e03dcdd526eb3
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-12 16:11:48 +0700

    Write GET & POST

--- src/domains/notification.js
+++ src/domains/notification.js
@@ -1,10 +1,16 @@
 const groupModel = require('../models/group');
+const notificationModel = require('../models/notification');
 
-// groupId -> [userId] -> token -> notify
 module.exports = {
+  // groupId -> [userId] -> token -> notify
   notifyNewMessage: (groupId, callback) => {
     groupModel.getUserFCMTokenSameGroup(groupId, (err, data) => {
       callback(err, data);
     });
   },
+
+  getNotifications: userId =>
+    notificationModel.getNotifications(userId),
+  addNotification: notification =>
+    notificationModel.addNotification(notification),
 };
