59f26f87152368be4e9f081b99c89bb1952877e8
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-12 07:55:30 +0700

    Fix missing tokens when sendToDevice()

--- src/domains/fcm.js
+++ src/domains/fcm.js
@@ -3,6 +3,8 @@ const admin = require('firebase-admin');
 module.exports = {
   sendMessageToDeviceWithTokens: (tokens, payload) => {
     if (tokens === undefined || tokens.length === 0) {
+      // Process the missing tokens
+    } else {
       admin.messaging().sendToDevice(tokens, payload);
     }
   },
