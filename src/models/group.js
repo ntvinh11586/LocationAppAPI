13952158f5c15234ad90df595bdb9725032b2609
Vinh Nguyen <ntvinh11586@users.noreply.github.com>
2017-06-24 14:33:25 +0700

    Delete non using routes (#119)

--- src/models/group.js
+++ src/models/group.js
@@ -84,131 +84,6 @@ function deleteTripPlan(groupId, callback) {
   setTripPlan(groupId, undefined, undefined, callback);
 }
 
-function updateStartingPoint(groupId, startTime, startLatlng, startAddress, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      group.start_time = startTime;
-      group.start_latlng = startLatlng;
-      group.start_address = startAddress;
-      group.save();
-
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        start_time: group.start_time,
-        start_address: group.start_address,
-        start_latlng: group.start_latlng,
-      });
-    }
-  });
-}
-
-function updateEndingPoint(groupId, endTime, endLatlng, endAddress, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      group.end_time = endTime;
-      group.end_latlng = endLatlng;
-      group.end_address = endAddress;
-      group.save();
-
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        end_time: group.end_time,
-        end_latlng: group.end_latlng,
-      });
-    }
-  });
-}
-
-function getStartingPoint(groupId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else if (group.start_latlng.lat !== undefined) {
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        start_time: group.start_time,
-        start_address: group.start_address,
-        start_latlng: group.start_latlng,
-      });
-    } else {
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        start_time: group.start_time,
-      });
-    }
-  });
-}
-
-function getEndingPoint(groupId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else if (group.end_latlng.lat !== undefined) {
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        end_time: group.end_time,
-        end_address: group.end_address,
-        end_latlng: group.end_latlng,
-      });
-    } else {
-      callback(null, {
-        group_id: group._id,
-        name: group.name,
-        end_time: group.end_time,
-      });
-    }
-  });
-}
-
 function addArrivingUser(groupId, userId, callback) {
   groupRepository.findById(groupId, (err, group) => {
     if (err) {
@@ -378,122 +253,6 @@ function deleteDestinationUser(groupId, userId, callback) {
   });
 }
 
-function deleteStartingPoint(groupId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      group.start_latlng = undefined;
-      group.arriving_users = undefined;
-      group.start_address = undefined;
-      group.save();
-      callback(null, {
-        group_id: groupId,
-      });
-    }
-  });
-}
-
-function deleteEndingPoint(groupId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      group.end_latlng = undefined;
-      group.destination_users = undefined;
-      group.end_address = undefined;
-      group.save();
-      callback(null, {
-        group_id: groupId,
-      });
-    }
-  });
-}
-
-const insert = (arr, index, newItem) => [
-  // part of the array before the specified index
-  ...arr.slice(0, index),
-  // inserted item
-  newItem,
-  // part of the array after the specified index
-  ...arr.slice(index),
-];
-
-function addStopover(groupId, latlng, position, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      const stopover = {
-        latlng,
-      };
-      group.stopovers = insert(group.stopovers, position, stopover);
-      group.save();
-      callback(null, {
-        stopover_id: group.stopovers.slice(-1 - position)[0]._id,
-        group_id: groupId,
-        latlng,
-        position,
-      });
-    }
-  });
-}
-
-function deleteStopover(groupId, stopoverId, callback) {
-  groupRepository.findById(groupId, (err, group) => {
-    if (err) {
-      callback(err, {
-        status_code: 422,
-        success: false,
-        status_message: err.message,
-      });
-    } if (group == null) {
-      callback(new Error('422'), {
-        status_code: 422,
-        success: false,
-        status_message: 'Group not found.',
-      });
-    } else {
-      group.stopovers.pull(stopoverId);
-      group.save();
-      callback(null, {
-        stopover_id: stopoverId,
-        group_id: groupId,
-      });
-    }
-  });
-}
-
 function addUserIntoStopover(groupId, userId, stopoverId, callback) {
   groupRepository.findById(groupId, (err, group) => {
     if (err) {
@@ -526,31 +285,6 @@ function addUserIntoStopover(groupId, userId, stopoverId, callback) {
       });
     }
   });
-}
-
-function getStopovers(groupId, callback) {
-  groupRepository.findById(groupId)
-    .populate({ path: 'stopovers.users', model: 'User', select: 'username' })
-    .exec((err, group) => {
-      if (err) {
-        callback(err, {
-          status_code: 422,
-          success: false,
-          status_message: err.message,
-        });
-      } if (group == null) {
-        callback(new Error('422'), {
-          status_code: 422,
-          success: false,
-          status_message: 'Group not found.',
-        });
-      } else {
-        callback(null, {
-          group_id: groupId,
-          stopovers: group.stopovers,
-        });
-      }
-    });
 }
 
 function deleteUserIntoStopover(groupId, userId, stopoverId, callback) {
@@ -814,30 +548,28 @@ function pushOneUserIntoGroupByGroupId(groupId, { userId }) {
 }
 
 module.exports = {
+  // trip plan
   setTripPlan,
   updateTripPlan,
   deleteTripPlan,
-  updateStartingPoint,
-  getStartingPoint,
-  getEndingPoint,
+  // starting point
   addArrivingUser,
   getArrivingUsers,
   deleteArrivingUser,
+  // destinational user
   addDestinationUser,
-  updateEndingPoint,
   getDestinationUsers,
   deleteDestinationUser,
-  deleteStartingPoint,
-  deleteEndingPoint,
-  addStopover,
-  deleteStopover,
+  // stopovers
   addUserIntoStopover,
-  getStopovers,
   deleteUserIntoStopover,
+  // route
   addRoute,
   getRoute,
   deleteRoute,
+  // fcm
   getUserFCMTokenSameGroup,
+  // Migrate data
   migrateFromGroupToRouteModel,
   migrateFromRouteToGroupModel,
 
