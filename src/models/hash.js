c8a3b1116fb461c2184c2392659c08204f13cc70
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-25 14:38:20 +0700

    Configure hash

---
+++ src/models/hash.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const config = require('../config');
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+function sha512(password, salt) {
+  const hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  const value = hash.digest('hex');
+  return { salt, passwordHash: value };
+}
+
+function saltHashPassword(userpassword) {
+  const salt = config.passwordHashKey;
+  const passwordData = sha512(userpassword, salt);
+  return passwordData.passwordHash;
+}
+
+console.log(saltHashPassword('1234567890'));
