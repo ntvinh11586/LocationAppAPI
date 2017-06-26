36bc1439e1334080817f5d430493393314db709c
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-27 01:54:09 +0700

    Goodbye cluster

--- tools/socketio_tester.js
+++ tools/socketio_tester.js
@@ -2,10 +2,10 @@ const io = require('socket.io-client');
 
 // const BASE_URL = 'http://localhost:3000';
 const BASE_URL = 'https://stormy-woodland-18039.herokuapp.com';
-const NAMESPACE = 'chats';
-const URL = `${BASE_URL}/${NAMESPACE}`;
+// const NAMESPACE = 'chats';
+const URL = `${BASE_URL}/`;
 
-const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwidXNlcl9pZCI6IjU5MjZiZmJjZjk3NjM3MzZhMDhmZDQ0ZSIsImlhdCI6MTQ5NTcxMTY3NiwiZXhwIjoxNTAwODk1Njc2fQ.3lDwP9-Hk5GjlDYvXKfw8s1XicTKSCP6TaZpWDRJWDQ';
+// const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwidXNlcl9pZCI6IjU5MjZiZmJjZjk3NjM3MzZhMDhmZDQ0ZSIsImlhdCI6MTQ5NTcxMTY3NiwiZXhwIjoxNTAwODk1Njc2fQ.3lDwP9-Hk5GjlDYvXKfw8s1XicTKSCP6TaZpWDRJWDQ';
 const QUERY = '594fe500d82fc300113f7280';
 
 const EMIT = process.argv[2] || 'MISSING_EMIT';
@@ -17,26 +17,12 @@ const socket = io.connect(BASE_URL, {
   query: `group_id=${QUERY}`,
 });
 
-console.log(URL);
 socket.on('connect', () => {
   console.log('EMIT', EMIT);
   console.log('EMIT_CALLBACK', EMIT_CALLBACK);
-  console.log(URL);
   socket
-  .emit('authenticate', { token: USER_TOKEN })
-  .on('authenticated', () => {
-    console.log('authenticated!');
-    console.log(requestData);
-    socket
-      .emit(EMIT, JSON.stringify((requestData)))
-      .on(EMIT_CALLBACK, (data) => {
-        console.log(data);
-      });
-  })
-  .on('unauthorized', (msg) => {
-    console.log('unauthorized!', msg);
-  })
-  .on('error', (data) => {
-    console.log(data);
-  });
+    .emit(EMIT, JSON.stringify((requestData)))
+    .on(EMIT_CALLBACK, (data) => {
+      console.log(data);
+    });
 });
