9edbca8e5f88c605a8d5aad2db61a9847c50aeea
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-27 02:07:26 +0700

    Reverse to base socketio_tester.js

--- tools/socketio_tester.js
+++ tools/socketio_tester.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 
-// const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://stormy-woodland-18039.herokuapp.com';
+const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'https://stormy-woodland-18039.herokuapp.com';
 const NAMESPACE = 'chats';
 const URL = `${BASE_URL}/${NAMESPACE}`;
 
@@ -13,16 +13,29 @@ const EMIT_CALLBACK = `${EMIT}_callback`;
 
 const requestData = require('./request_data.json');
 
-const socket = io.connect(BASE_URL, {
+const socket = io.connect(URL, {
   query: `group_id=${QUERY}`,
 });
 
 socket.on('connect', () => {
   console.log('EMIT', EMIT);
   console.log('EMIT_CALLBACK', EMIT_CALLBACK);
+
   socket
-    .emit(EMIT, JSON.stringify((requestData)))
-    .on(EMIT_CALLBACK, (data) => {
-      console.log(data);
-    });
+  .emit('authenticate', { token: USER_TOKEN })
+  .on('authenticated', () => {
+    console.log('authenticated!');
+    console.log(requestData);
+    socket
+      .emit(EMIT, JSON.stringify((requestData)))
+      .on(EMIT_CALLBACK, (data) => {
+        console.log(data);
+      });
+  })
+  .on('unauthorized', (msg) => {
+    console.log('unauthorized!', msg);
+  })
+  .on('error', (data) => {
+    console.log(data);
+  });
 });
