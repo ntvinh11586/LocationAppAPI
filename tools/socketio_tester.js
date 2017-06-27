fb665e3c20f2e77121e5d3d35f156bea61a6ad15
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-27 08:18:34 +0700

    Reverse auth

--- tools/socketio_tester.js
+++ tools/socketio_tester.js
@@ -1,9 +1,9 @@
 const io = require('socket.io-client');
 
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://stormy-woodland-18039.herokuapp.com';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://stormy-woodland-18039.herokuapp.com';
 const NAMESPACE = 'chats';
-// const URL = `${BASE_URL}/${NAMESPACE}`;
+const URL = `${BASE_URL}/${NAMESPACE}`;
 
 const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwidXNlcl9pZCI6IjU5MjZiZmJjZjk3NjM3MzZhMDhmZDQ0ZSIsImlhdCI6MTQ5NTcxMTY3NiwiZXhwIjoxNTAwODk1Njc2fQ.3lDwP9-Hk5GjlDYvXKfw8s1XicTKSCP6TaZpWDRJWDQ';
 const QUERY = '594fe500d82fc300113f7280';
@@ -13,8 +13,7 @@ const EMIT_CALLBACK = `${EMIT}_callback`;
 
 const requestData = require('./request_data.json');
 
-const socket = io.connect(BASE_URL, {
-  resource: 'chats/socket.io',
+const socket = io.connect(URL, {
   query: `group_id=${QUERY}`,
 });
 
@@ -22,21 +21,21 @@ socket.on('connect', () => {
   console.log('EMIT', EMIT);
   console.log('EMIT_CALLBACK', EMIT_CALLBACK);
 
-  // socket
-  // .emit('authenticate', { token: USER_TOKEN })
-  // .on('authenticated', () => {
-  //   console.log('authenticated!');
-  //   console.log(requestData);
   socket
-    .emit(EMIT, JSON.stringify((requestData)))
-    .on(EMIT_CALLBACK, (data) => {
-      console.log(data);
-    });
-  // })
-  // .on('unauthorized', (msg) => {
-  //   console.log('unauthorized!', msg);
-  // })
-  // .on('error', (data) => {
-  //   console.log(data);
-  // });
+  .emit('authenticate', { token: USER_TOKEN })
+  .on('authenticated', () => {
+    console.log('authenticated!');
+    console.log(requestData);
+    socket
+      .emit(EMIT, JSON.stringify((requestData)))
+      .on(EMIT_CALLBACK, (data) => {
+        console.log(data);
+      });
+  })
+  .on('unauthorized', (msg) => {
+    console.log('unauthorized!', msg);
+  })
+  .on('error', (data) => {
+    console.log(data);
+  });
 });
