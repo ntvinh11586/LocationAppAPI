e5109c274eabfbc7d2c71507b227b8480c4b3020
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-27 15:46:38 +0700

    Complete implement

---
+++ tools/cached_data_management.js
@@ -0,0 +1,32 @@
+const redis = require('redis');
+
+// User Cache Database
+const client = redis.createClient('redis://redis-10039.c9.us-east-1-4.ec2.cloud.redislabs.com:10039/');
+
+if (process.argv[2] === 'get_all') {
+  client.keys('*', (err, keys) => {
+    if (err) {
+      return console.log(err);
+    }
+    for (let i = 0, len = keys.length; i < len; i += 1) {
+      client.get(keys[i], (error, data) => {
+        console.log('get_all', keys[i], data);
+      });
+    }
+    return null;
+  });
+} else if (process.argv[2] === 'set') {
+  client.set(process.argv[3] || '', process.argv[4], () => {
+    client.get(process.argv[3] || '', (error, value) => {
+      console.log('set', process.argv[3] || '', value);
+    });
+  });
+} else if (process.argv[2] === 'get') {
+  client.get(process.argv[3] || '', (error, value) => {
+    console.log('get', process.argv[3] || '', value);
+  });
+} else if (process.argv[2] === 'delete_all') {
+  client.flushdb((err, succeeded) => {
+    console.log('delete_all', succeeded); // will be true if successfull
+  });
+}
