7721b94c046d0282bd399ba230284fb22755b824
Vinh Nguyen <ntvinh11586@users.noreply.github.com>
2017-06-30 16:23:32 +0700

    Enhance GET notification lists in order & in Sockets (#138)

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,12 +1,15 @@
 const map = require('./map');
 const chat = require('./chat');
 const demo = require('./demo');
+const notification = require('./notification');
 
 module.exports = (io) => {
   const chatNamespace = io.of('/chats');
   const mapNamespace = io.of('/maps');
+  const notificationNamespace = io.of('/notifications');
 
-  chat(chatNamespace);
   demo(io);
+  chat(chatNamespace);
   map(mapNamespace);
+  notification(notificationNamespace);
 };
