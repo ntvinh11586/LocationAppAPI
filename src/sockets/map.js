7db82dbb8e420a24333d290649ec8c48bbe4e193
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-30 15:28:38 +0700

    Remove the transport in sockets

--- src/sockets/map.js
+++ src/sockets/map.js
@@ -10,158 +10,6 @@ const cacheDomain = require('../domains/cache');
 const groupDomain = require('../domains/group');
 const userModel = require('../models/user');
 
-function getAllUsersLocation(groupInfo, callback) {
-  const groupJSON = JSON.parse(groupInfo);
-  const groupId = groupJSON.group_id;
-  latlngModel.getUsersLatlng(groupId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function getUserLocation(body) {
-  const { user_id: userId } = JSON.parse(body);
-  return locationDomain.getUserCurrentLocation({ userId });
-}
-
-function addDestinationUser(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const userId = groupInfoJSON.user_id;
-  groupModel.addDestinationUser(groupId, userId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function deleteArrivingUser(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const userId = groupInfoJSON.user_id;
-  groupModel.deleteArrivingUser(groupId, userId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function deleteDestinationUser(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const userId = groupInfoJSON.user_id;
-  groupModel.deleteDestinationUser(groupId, userId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function getArrivingUsers(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  groupModel.getArrivingUsers(groupId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function getDestinationUsers(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  groupModel.getDestinationUsers(groupId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function addUserIntoStopover(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const userId = groupInfoJSON.user_id;
-  const stopoverId = groupInfoJSON.stopover_id;
-  groupModel.addUserIntoStopover(groupId, userId, stopoverId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function deleteUserIntoStopover(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const userId = groupInfoJSON.user_id;
-  const stopoverId = groupInfoJSON.stopover_id;
-  groupModel.deleteUserIntoStopover(groupId, userId, stopoverId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function addRoute(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const startLatlng = groupInfoJSON.start_latlng;
-  const endLatlng = groupInfoJSON.end_latlng;
-  const stopovers = groupInfoJSON.stopovers;
-  const startRadius = groupInfoJSON.start_radius;
-  const endRadius = groupInfoJSON.end_radius;
-  groupModel.addRoute(groupId,
-    startLatlng,
-    endLatlng,
-    startRadius,
-    endRadius,
-    stopovers,
-    (err, data) => {
-      callback(err, data);
-    });
-}
-
-function getRoute(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  groupModel.getRoute(groupId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-function deleteRoute(groupInfo, callback) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  groupModel.deleteRoute(groupId, (err, data) => {
-    callback(err, data);
-  });
-}
-
-// Appointment
-function getAppointments(groupInfo) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  return appointmentDomain.getAppointments({ groupId });
-}
-
-function addAppointment(groupInfo) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const address = groupInfoJSON.address;
-  const latlng = groupInfoJSON.latlng;
-  const startTime = groupInfoJSON.start_time;
-  const endTime = groupInfoJSON.end_time;
-  const radius = groupInfoJSON.radius;
-  return appointmentDomain.addAppointment({ groupId, latlng, address, startTime, endTime, radius });
-}
-
-function deleteAppointment(groupInfo) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const appointmentId = groupInfoJSON.appointment_id;
-  return appointmentDomain.deleteAppointment({ groupId, appointmentId });
-}
-
-function addUserToAppointment(groupInfo) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const appointmentId = groupInfoJSON.appointment_id;
-  const userId = groupInfoJSON.user_id;
-  return appointmentDomain.addUserToAppointment({ groupId, appointmentId, userId });
-}
-
-function deleteUserToAppointment(groupInfo) {
-  const groupInfoJSON = JSON.parse(groupInfo);
-  const groupId = groupInfoJSON.group_id;
-  const appointmentId = groupInfoJSON.appointment_id;
-  const userId = groupInfoJSON.user_id;
-  return appointmentDomain.deleteUserFromAppointment({ groupId, appointmentId, userId });
-}
-
 function groupLocation(mapNamespace) {
   mapNamespace
     .on('connection', socketioJwt.authorize({
@@ -202,14 +50,16 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('get_latlng', (body) => {
-        getUserLocation(body)
+        const { user_id: userId } = JSON.parse(body);
+        locationDomain.getUserCurrentLocation({ userId })
           .then((data) => {
             socket.emit('get_latlng_callback', data);
           });
       });
 
       socket.on('get_latlngs', (body) => {
-        getAllUsersLocation(body, (err, data) => {
+        const { group_id: groupId } = JSON.parse(body);
+        latlngModel.getUsersLatlng(groupId, (err, data) => {
           socket.emit('get_latlngs_callback', data);
         });
       });
@@ -249,7 +99,7 @@ function groupLocation(mapNamespace) {
       socket.on('delete_arriving_user', (body) => {
         const { user_id: userId, group_id: groupId } = JSON.parse(body);
 
-        deleteArrivingUser(body, (err, data) => {
+        groupModel.deleteArrivingUser(groupId, userId, (err, data) => {
           socket.emit('delete_arriving_user_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('delete_arriving_user_callback', data);
@@ -278,13 +128,15 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('get_arriving_users', (groupInfo) => {
-        getArrivingUsers(groupInfo, (err, data) => {
+        const { group_id: groupId } = JSON.parse(groupInfo);
+        groupModel.getArrivingUsers(groupId, (err, data) => {
           socket.emit('get_arriving_users_callback', data);
         });
       });
 
       socket.on('get_destination_users', (groupInfo) => {
-        getDestinationUsers(groupInfo, (err, data) => {
+        const { group_id: groupId } = JSON.parse(groupInfo);
+        groupModel.getDestinationUsers(groupId, (err, data) => {
           socket.emit('get_destination_users_callback', data);
         });
       });
@@ -292,7 +144,7 @@ function groupLocation(mapNamespace) {
       socket.on('add_destination_user', (body) => {
         const { user_id: userId, group_id: groupId } = JSON.parse(body);
 
-        addDestinationUser(body, (err, data) => {
+        groupModel.addDestinationUser(groupId, userId, (err, data) => {
           socket.emit('add_destination_user_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('add_destination_user_callback', data);
@@ -323,7 +175,7 @@ function groupLocation(mapNamespace) {
       socket.on('delete_destination_user', (body) => {
         const { user_id: userId, group_id: groupId } = JSON.parse(body);
 
-        deleteDestinationUser(body, (err, data) => {
+        groupModel.deleteDestinationUser(groupId, userId, (err, data) => {
           socket.emit('delete_destination_user_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('delete_destination_user_callback', data);
@@ -352,9 +204,13 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('add_user_into_stopover', (body) => {
-        const { user_id: userId, group_id: groupId } = JSON.parse(body);
+        const {
+          user_id: userId,
+          group_id: groupId,
+          stopover_id: stopoverId,
+        } = JSON.parse(body);
 
-        addUserIntoStopover(body, (err, data) => {
+        groupModel.addUserIntoStopover(groupId, userId, stopoverId, (err, data) => {
           socket.emit('add_user_into_stopover_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('add_user_into_stopover_callback', data);
@@ -383,9 +239,13 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('delete_user_into_stopover', (body) => {
-        const { user_id: userId, group_id: groupId } = JSON.parse(body);
+        const {
+          user_id: userId,
+          group_id: groupId,
+          stopover_id: stopoverId,
+        } = JSON.parse(body);
 
-        deleteUserIntoStopover(body, (err, data) => {
+        groupModel.deleteUserIntoStopover(groupId, userId, stopoverId, (err, data) => {
           socket.emit('delete_user_into_stopover_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('delete_user_into_stopover_callback', data);
@@ -414,25 +274,38 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('add_route', (body) => {
-        const { group_id: groupId } = JSON.parse(body);
+        const {
+          group_id: groupId,
+          start_latlng: startLatlng,
+          end_latlng: endLatlng,
+          stopovers,
+          start_radius: startRadius,
+          end_radius: endRadius,
+        } = JSON.parse(body);
 
-        addRoute(body, (err, data) => {
-          socket.emit('add_route_callback', data);
-          socket.join(groupId);
-          socket.broadcast.to(groupId).emit('add_route_callback', data);
-        });
+        groupModel.addRoute(groupId,
+          startLatlng,
+          endLatlng,
+          startRadius,
+          endRadius,
+          stopovers,
+          (err, data) => {
+            socket.emit('add_route_callback', data);
+            socket.join(groupId);
+            socket.broadcast.to(groupId).emit('add_route_callback', data);
+          });
       });
 
       socket.on('get_route', (groupInfo) => {
-        getRoute(groupInfo, (err, data) => {
+        const { group_id: groupId } = JSON.parse(groupInfo);
+        groupModel.getRoute(groupId, (err, data) => {
           socket.emit('get_route_callback', data);
         });
       });
 
       socket.on('delete_route', (body) => {
         const { group_id: groupId } = JSON.parse(body);
-
-        deleteRoute(body, (err, data) => {
+        groupModel.deleteRoute(groupId, (err, data) => {
           socket.emit('delete_route_callback', data);
           socket.join(groupId);
           socket.broadcast.to(groupId).emit('delete_route_callback', data);
@@ -441,16 +314,24 @@ function groupLocation(mapNamespace) {
 
       // Appointment
       socket.on('get_appointments', (groupInfo) => {
-        getAppointments(groupInfo)
+        const { group_id: groupId } = JSON.parse(groupInfo);
+        appointmentDomain.getAppointments({ groupId })
           .then((data) => {
             socket.emit('get_appointments_callback', data);
           });
       });
 
       socket.on('add_appointment', (body) => {
-        const { group_id: groupId } = JSON.parse(body);
+        const {
+          group_id: groupId,
+          address,
+          latlng,
+          start_time: startTime,
+          end_time: endTime,
+          radius,
+        } = JSON.parse(body);
 
-        addAppointment(body)
+        appointmentDomain.addAppointment({ groupId, latlng, address, startTime, endTime, radius })
           .then((data) => {
             socket.emit('add_appointment_callback', data);
             socket.join(groupId);
@@ -459,9 +340,12 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('delete_appointment', (body) => {
-        const { group_id: groupId } = JSON.parse(body);
+        const {
+          group_id: groupId,
+          appointment_id: appointmentId,
+        } = JSON.parse(body);
 
-        deleteAppointment(body)
+        appointmentDomain.deleteAppointment({ groupId, appointmentId })
           .then((data) => {
             socket.emit('delete_appointment_callback', data);
             socket.join(groupId);
@@ -470,9 +354,13 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('add_user_to_appointment', (body) => {
-        const { user_id: userId, group_id: groupId } = JSON.parse(body);
+        const {
+          user_id: userId,
+          group_id: groupId,
+          appointment_id: appointmentId,
+        } = JSON.parse(body);
 
-        addUserToAppointment(body)
+        appointmentDomain.addUserToAppointment({ groupId, appointmentId, userId })
           .then((data) => {
             socket.emit('add_user_to_appointment_callback', data);
             socket.join(groupId);
@@ -507,9 +395,13 @@ function groupLocation(mapNamespace) {
       });
 
       socket.on('delete_user_from_appointment', (body) => {
-        const { user_id: userId, group_id: groupId } = JSON.parse(body);
+        const {
+          user_id: userId,
+          group_id: groupId,
+          appointment_id: appointmentId,
+        } = JSON.parse(body);
 
-        deleteUserToAppointment(body)
+        appointmentDomain.deleteUserFromAppointment({ groupId, appointmentId, userId })
           .then((data) => {
             socket.emit('delete_user_from_appointment_callback', data);
             socket.join(groupId);
