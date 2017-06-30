b1024cfe4dc57a9eea6033b42f248659c9cfeaf2
ntvinh11586 <ntvinh.11586@gmail.com>
2017-06-30 16:00:13 +0700

    Sort in decreased notifications

--- src/domains/notification.js
+++ src/domains/notification.js
@@ -12,8 +12,12 @@ module.exports = {
   getNotifications: userId =>
     notificationModel.getNotifications(userId)
       .then((data) => {
-        return { notifications: data };
-      }),
+        // -1 for notification without date
+        // We need to sort in decreased time
+        data.sort((p1, p2) => (p2.date || -1) - (p1.date || -1));
+        return data;
+      })
+      .then(data => ({ notifications: data })),
 
   addNotification: notification =>
     notificationModel.addNotification(notification),
