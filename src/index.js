ec7e69343a0658580be7dd78ecf78f8859876d05
ntvinh11586 <ntvinh.11586@gmail.com>
2017-07-09 15:42:59 +0700

    Remove redis-socketio

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const http = require('http');
 const socketio = require('socket.io');
 const router = require('./controllers');
 const socket = require('./sockets');
-const redis = require('redis').createClient;
 const adapter = require('socket.io-redis');
 
 // Create an IO Server instance.
@@ -10,14 +9,6 @@ const ioServer = (app) => {
   // Configure server with Socket.io
   const server = http.Server(app);
   const io = socketio(server);
-  const pubClient = redis(12471, 'pub-redis-12471.ap-northeast-1-2.1.ec2.garantiadata.com', {});
-  const subClient = redis(12471, 'pub-redis-12471.ap-northeast-1-2.1.ec2.garantiadata.com', {
-    return_buffers: true,
-  });
-  io.adapter(adapter({
-    pubClient,
-    subClient,
-  }));
   socket(io);
 
   return server;
